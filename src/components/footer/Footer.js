5c4aa4723ff524b3751e2b1a3c2d6a379ce34701
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2020-12-16 22:29:18 +0100

    Finish the footer, the homepage fully responsive done!

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
+import Button from '../button/Button';
+
+import LogoWhite from '../../assets/logo-white.svg';
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <div className='container footer_container'>
-                footer
+                <div className='footer_logo'>
+                    <img src={LogoWhite} alt='Arch Studio Logo' />
+                </div>
+                <div className='footer_nav'>
+                    <ul className='footer_list'>
+                        <li><Link to='/portfolio' className='footer_link'>Portfolio</Link></li>
+                        <li><Link to='/about' className='footer_link'>About Us</Link></li>
+                        <li><Link to='/contact' className='footer_link'>Contact</Link></li>
+                    </ul>
+                </div>
+                <div className='footer_btn'>
+                    <Button to='/portfolio' cName='btn btn_lg'>See Our Portfolio</Button>
+                </div>
             </div>
         </footer>
     )
