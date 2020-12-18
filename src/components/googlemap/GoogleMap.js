64b5c12a3f5cc148ca62aac47cb9913555cd4f27
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2020-12-18 08:53:30 +0100

    Fix google map api

--- src/components/googlemap/GoogleMap.js
+++ src/components/googlemap/GoogleMap.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const GoogleMap = () => {
+const GoogleMap = ({text}) => {
     return (
         <section className='map'>
             <div className='container map_container'>
-                MAP
+                {text}
             </div>
         </section>
     )
 }
 
-export default GoogleMap;
+class SimpleMap extends Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
+   
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '500px', width: '80%', margin: '0 auto 100px', }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: '' }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <GoogleMap
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My office"
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+   
+export default SimpleMap;
